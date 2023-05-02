e7af5fc4c8e7305dc2e3ad1b5f037c5416e3afb1
Ethanwelz <126813033+Ethanwelz@users.noreply.github.com>
2023-05-02 14:15:44 -0400

    Add files via upload

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,43 +1,33 @@
 import { useState, useEffect, Fragment } from 'react';
-import { useSelector } from 'react-redux'; 
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import Spinner from '../../components/spinner/spinner.component';
 
-import { 
-    selectCategoriesIsLoading, 
-    selectCategoriesMap 
-} from '../../store/categories/category.selector';
+import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
-   const { category } = useParams();
-   const categoriesMap = useSelector(selectCategoriesMap);
-   const isLoading = useSelector(selectCategoriesIsLoading);
-   const [products, setProducts] = useState(categoriesMap[category]);
+  const { category } = useParams();
+  const categoriesMap = useSelector(selectCategoriesMap);
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-   useEffect(() => {
+  useEffect(() => {
     setProducts(categoriesMap[category]);
-   }, [category, categoriesMap]);
+  }, [category, categoriesMap]);
 
-   return (
+  return (
     <Fragment>
-        <Title>{category.toUpperCase()}</Title>
-        {
-            isLoading ? ( 
-                <Spinner /> 
-            ) : (
-                <CategoryContainer>
-                    {products && 
-                        products.map((product) => (
-                            <ProductCard key={product.id} product={product} />
-                    ))}
-                </CategoryContainer>
-            )}
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </CategoryContainer>
     </Fragment>
-   );
+  );
 };
 
 export default Category;
