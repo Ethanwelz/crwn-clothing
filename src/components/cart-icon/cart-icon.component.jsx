3310067064ff1dd794bc94730abcfdc3a49d72ad
Ethanwelz <126813033+Ethanwelz@users.noreply.github.com>
2023-05-02 14:14:19 -0400

    Add files via upload

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,13 @@ import {
     selectCartCount, 
     selectIsCartOpen 
 } from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.action';
-
+import { setIsCartOpen } from '../../store/cart/cart.reducer';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
     const dispatch = useDispatch();
-
     const cartCount = useSelector(selectCartCount);
     const isCartOpen = useSelector(selectIsCartOpen);
 
