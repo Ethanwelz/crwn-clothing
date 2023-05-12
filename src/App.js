5664822a4ffa8104e1a176d9eb0c173e53cb20ae
Ethanwelz <126813033+Ethanwelz@users.noreply.github.com>
2023-05-12 13:21:20 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,30 +3,18 @@ import { useDispatch } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
 
+import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.reducer';
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
+import { checkUserSession } from './store/user/user.action';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-
-      const pickedUser = 
-        user && (({accessToken, email}) => ({ accessToken, email }))(user);
-
-      dispatch(setCurrentUser(pickedUser));
-    });
-
-    return unsubscribe;
+    dispatch(checkUserSession());
   }, []);
 
   return (
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,27 +1,29 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { 
-    selectCartCount, 
-    selectIsCartOpen 
+import {
+  selectCartCount,
+  selectIsCartOpen,
 } from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.reducer';
+import { setIsCartOpen } from '../../store/cart/cart.action.js';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
-    const dispatch = useDispatch();
-    const cartCount = useSelector(selectCartCount);
-    const isCartOpen = useSelector(selectIsCartOpen);
+  const dispatch = useDispatch();
 
-    const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
-    
-    return (
-        <CartIconContainer onClick={toggleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon' />
-            <ItemCount>{cartCount}</ItemCount>
-        </CartIconContainer>
-    );
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
 };
 
 export default CartIcon;
