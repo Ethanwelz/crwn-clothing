96d0574d79e54aa9e1fe02faa1c57870bb2fa514
Ethanwelz <126813033+Ethanwelz@users.noreply.github.com>
2023-05-13 19:44:03 -0400

    Add files via upload

---
+++ src/store/middleware/logger.ts
@@ -0,0 +1,17 @@
+import { Middleware } from "redux";
+
+import { RootState } from "../store";
+
+export const loggerMiddleWare: Middleware<{}, RootState> = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('cuurentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+};
