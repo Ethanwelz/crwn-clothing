e2d4007dc821fa81e5254d760215f1441b59d256
Ethanwelz <126813033+Ethanwelz@users.noreply.github.com>
2023-05-14 22:26:48 -0400

    Add files via upload

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,31 +8,31 @@ import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../store/cart/cart.selector';
 
 import {
-    CartDropdownContainer, 
-    EmptyMessage, 
-    CartItems
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
 } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
-    const cartItems = useSelector(selectCartItems);
-    const navigate = useNavigate();
+  const cartItems = useSelector(selectCartItems);
+  const navigate = useNavigate();
 
-    const goToCheckoutHandler = () => {
-        navigate('/checkout')
-    }
+  const goToCheckoutHandler = useCallback(() => {
+    navigate('/checkout');
+  }, []);
 
-    return (
-        <CartDropdownContainer>
-            <CartItems>
-                {cartItems.length ? (
-                    cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
-                ) : (
-                    <EmptyMessage>Your cart is empty</EmptyMessage>
-                )}
-            </CartItems>
-            <Button onClick={goToCheckoutHandler}>Check Out</Button>
-        </CartDropdownContainer>
-    )
-}
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>Checkout</Button>
+    </CartDropdownContainer>
+  );
+};
 
 export default CartDropdown;
